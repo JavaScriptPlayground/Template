6bf19cb232258941720ae119a14ad2a19be173c6
Mqxx <62719703+Mqxx@users.noreply.github.com>
2024-10-01 09:02:04 +0200

    Configuration update

--- src/client/_scripts/index.tsx
+++ src/client/_scripts/index.tsx
@@ -1,20 +1,5 @@
 import { render } from "@solid-js/web";
-import { createEffect, createSignal, type JSX } from "@solid-js";
-
-function Counter() : JSX.Element {
-  const [count, setCount] = createSignal(1);
-  function increment() : void {setCount(count => count + 1)};
-
-  createEffect(() => {
-    console.log("Count has changed to:", count());
-  });
-
-  return (
-    <button type="button" onClick={increment}>
-      {count()}
-    </button>
-  );
-}
+import { Counter } from '../_components/counter.tsx';
 
 // deno-lint-ignore no-non-null-assertion no-undef
 render(() => <Counter />, document.getElementById("app")!);
